87fd6b18ef5f8710ea1fea29b706755f38bfdcdb
eonnine <eonnine@github.com>
2023-01-03 13:52:02 +0900

    first commit

---
+++ src/main/java/spring/audit/MybatisAuditApplication.java
@@ -0,0 +1,13 @@
+package spring.audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisAuditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisAuditApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/audit/MybatisAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisAuditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
